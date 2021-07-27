e4323b54a685cd8c36fe36fd17b5613ed750c670
Davis <daviskipkiruitoo@gmail.com>
2021-07-27 13:20:50 +0300

    added randomSymbol to this.render

--- matrix_rain/sketch.js
+++ matrix_rain/sketch.js
@@ -2,7 +2,7 @@
 var symbols = [];
 
 // create an array of x vvalues to hold our widths
-var widtharr = _.range(0, window.innerWidth, 2);
+var widtharr = _.range(0, window.innerWidth);
 
 // set our symbol size
 var symbolSize = 30;
@@ -13,6 +13,7 @@ function setup() {
         window.innerWidth,
         window.innerHeight
     );
+
     background(0);
 
     // create a symbol objects from easch of our width points
@@ -43,22 +44,28 @@ function draw(){
 }
 // create a symbol class
 function JSymbol(x, y, speed){
-        this.x = x;
+    this.x = x;
     this.y = y;
     this.value;
     this.speed = speed;
+    this.switchInterval = round(random(12, 20));
 
     this.setToRandomSymbol = function () {
         // get a random japanese / katakana unicode character
-        this.value = String.fromCharCode(
+
+        if (frameCount % this.switchInterval == 0) {
+            this.value = String.fromCharCode(
             0x30A0 +  round(random(0, 96))
         );
+        }
+        
     }
 
     this.render = function(){
         fill(0, 255, 70);
         text(this.value, this.x, this.y);
         this.rain()
+        this.setToRandomSymbol();
     }
 
     this.rain = function(){
